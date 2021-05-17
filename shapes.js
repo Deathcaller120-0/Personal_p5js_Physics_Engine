5061d3b0d6a27edf54f7aae4b5ffadc7a0961282
Deathcaller120-0 <49953301+Deathcaller120-0@users.noreply.github.com>
2021-05-17 10:03:18 -0700

    adding debugging and fixed super calls

--- shapes.js
+++ shapes.js
@@ -1,5 +1,5 @@
 class Shape {
-  constructor(x, y, vx, vy, w, h, _color, shapeType, bounceLoss = 0.5, vectors = []){
+  constructor(x=50, y=50, vx=0, vy=0, w=10, h=10, _color="#f00", shapeType="rect", bounceLoss = 0.5, vectors = []){
     this.pos = createVector(x, y);
     this.vel = createVector(vx, vy);
     this.size = createVector(w, h);
@@ -13,18 +13,22 @@ class Shape {
     this.type = shapeType;
     
     window.alert("created shape of type:" + this.type);
+    window.alert("pos:" + this.pos.x + ", " + this.pos.y);
   }
   
-  update(){
+  subUpdate(){
     window.alert("updating shape");
     
     this.vel.y += phys.gravity;
     if(!rectRect(this.pos.x-this.size.x/2, this.pos.y-this.size.y/2, this.size.x/2, this.size.y/2, 0, 0, width, height)){
+      window.alert("Out of bounds, Calculating");
       // its outside the canvas bounds
       let x = this.vel.x;
       let y = this.vel.y;
       let sx = this.size.x/2; 
       let sy = this.size.y/2;
+      
+      window.alert("vars set");
       
       // if it has enough velocity to escape the bounds, don't let it
       if (y+sy > height || y-sy < 0){
@@ -35,29 +39,36 @@ class Shape {
         this.invertVel(1);
       }
       
+      window.alert("Setting x,y to pos.x, pos.y");
+      
       // set x, y to shapes position
       x = this.pos.x;
       y = this.pos.y;
       
       if (y+sy > height){
+        window.alert("bottom collsion");
         while (y+sy > height){
-          this.pos.y += -1e-4;
+          this.pos.y += -1e-2;
         }
       } else if (y-sy < 0){
+        window.alert("top collsion");
         while (y-sy < 0){
-          this.pos.y += 1e-4;
+          this.pos.y += 1e-2;
         }
       }
       
       if (x+sx > width){
+        window.alert("right collsion");
         while (y+sy > width){
-          this.pos.x += -1e-4;
+          this.pos.x += -1e-2;
         }
       } else if (x-sx < 0){
+        window.alert("left collsion")
         while (x-sx < 0){
-          this.pos.x += 1e-4;
+          this.pos.x += 1e-2;
         }
       }
+      window.alert("Exiting collsion checker");
     }
     this.pos.add(this.vel);
   }
@@ -67,6 +78,7 @@ class Shape {
   }
   
   invertVel(isX){
+    window.alert("inverted vel for:" + (isX ? "x" : "y"));
     if (isX){
       this.vel.x += -this.vel.x * this.bounce;
       this.vel.x *= -1;
@@ -84,16 +96,16 @@ class CircleShape extends Shape {
   }
   
   update(){
-    super.update();
+    this.subUpdate();
   }
   
   render(){
     window.alert("circle rendered");
     push()
-    rotate(super.rot.x);
-    translate(super.pos.x, super.pos.y);
-    fill(super.color);
-    circle(0, 0, super.size.x);
+    rotate(this.rot.x);
+    translate(this.pos.x, this.pos.y);
+    fill(this.color);
+    circle(0, 0, this.size.x);
     pop()
   }
 }
