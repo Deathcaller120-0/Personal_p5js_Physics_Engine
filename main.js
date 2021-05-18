560c4f126adf666a217b6ba38256c64f964a68bb
Deathcaller120-0 <49953301+Deathcaller120-0@users.noreply.github.com>
2021-05-18 11:48:41 -0700

    changed Circle(class) to phys_Circle

--- main.js
+++ main.js
@@ -17,7 +17,9 @@ function setup(){
   
   createCanvas(500, 500);
   
-  physObj.push(new CircleShape(width/2, height/2, random(-10, 10), random(-10, 10), 50));
+  physObj.push(new phys_CircleShape(width/2, height/2, random(-10, 10), random(-10, 10), 50));
+  physObj.push(new phys_CircleShape(random(width), random(height), random(-20, 20), random(-20, 20), random(10, 50)));
+  physObj[physObj.length-1].setIndex = 1;
   
   physPreTick();
   
@@ -104,7 +106,7 @@ function spawn(){
   let vy = document.getElementById("spawnVelY").value;
   
   if (type == "circle"){
-    physObj.push(new Circle(x, y, vx, vy, sizeX)));
+    physObj.push(new phys_Circle(x, y, vx, vy, sizeX)));
   }
   physObj[physObj.length-1].setIndex(physObj.length-1);
 }
