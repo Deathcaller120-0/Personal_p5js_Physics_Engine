e1140f9025bb66e134998ec93f7a59ec94ced556
Deathcaller120-0 <49953301+Deathcaller120-0@users.noreply.github.com>
2021-05-20 14:45:36 -0700

    Update shapes.js

--- shapes.js
+++ shapes.js
@@ -81,50 +81,43 @@ class phys_Shape {
   
   collsionUpdate(){
     for (let i = 0; i < physObj.length; i++){
-      if (this.getIndex() == physObj[i].getIndex()) {continue};
+      if (this.getIndex() == physObj[i].getIndex()) {continue;}
       
       let other = physObj[i];
-      let type0 = this.getShapeType();
-      let type1 = other.getShapeType();
-    
       //window.alert("checking larger collsion");
       if (rectRect(other.pos.x - other.size.x, other.pos.y - other.size.y, other.size.x * 2, other.size.y * 2, this.pos.x - this.size.x/2, this.pos.y - this.size.y, this.size.x * 2, this.size.y * 2)){
-        if (type0 == "circle" || type1 == "circle"){
-          if (type0 == type1) {
-            if (polyPoly(this.hitPoints, other.hitPoints)){
-              let hits = [];
-              for (let i = 0; i < this.hitPoints.length; i++){
-                if (polyPoint(other.hitPoints, this.hitPoints[i].x, this.hitPoints[i].y)) { 
-                  hits.push(this.hitPoints[i].label);
-                  this.hitPoints[i].hit = 1;
-                  window.alert("Hit " + this.hitPoints[i].label + " on physObj[" + this.getIndex() + "]");
-                }
-              }
-            
-              let invY = 0;
-              let invX = 0;
-              for (let i = 0; i < hits.length; i++){
-                if ((hits[i] == "top-left" || hits[i] == "top-mid" || hits[i] == "top-right" || hits[i] == "bottom-left" || hits[i] == "bottom-mid" || hits[i] == "bottom-right") && invY == 0){
-                  invY = 1;
-                }
-                if ((hits[i] == "top-left" || hits[i] == "mid-left" || hits[i] == "top-right" || hits[i] == "bottom-left" || hits[i] == "bottom-right" || hits[i] == "mid-right") && invX == 0){
-                  invX = 1;
-                }
-                if (invX && invY){break;}
-              }
-            
-              if (invX){
-                this.invertVel(1);
-              }
-              if (invY){
-                this.invertVel(0);
-              }
-              if (invX || invY){
-                this.pos.add(this.vel);
+          if (polyPoly(this.hitPoints, other.hitPoints)){
+            let hits = [];
+            for (let i = 0; i < this.hitPoints.length; i++){
+              if (polyPoint(other.hitPoints, this.hitPoints[i].x, this.hitPoints[i].y)) { 
+                hits.push(this.hitPoints[i].label);
+                this.hitPoints[i].hit = 1;
+                window.alert("Hit " + this.hitPoints[i].label + " on physObj[" + this.getIndex() + "]");
               }
             }
+          
+            let invY = 0;
+            let invX = 0;
+            for (let i = 0; i < hits.length; i++){
+              if ((hits[i] == "top-left" || hits[i] == "top-mid" || hits[i] == "top-right" || hits[i] == "bottom-left" || hits[i] == "bottom-mid" || hits[i] == "bottom-right") && invY == 0){
+                invY = 1;
+              }
+              if ((hits[i] == "top-left" || hits[i] == "mid-left" || hits[i] == "top-right" || hits[i] == "bottom-left" || hits[i] == "bottom-right" || hits[i] == "mid-right") && invX == 0){
+                invX = 1;
+              }
+              if (invX && invY){break;}
+            }
+          
+            if (invX){
+              this.invertVel(1);
+            }
+            if (invY){
+              this.invertVel(0);
+            }
+            if (invX || invY){
+              this.pos.add(this.vel);
+            }
           }
-        }
       }
     }
   }
