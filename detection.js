5de90f84c25380ad020c9b1cd36e477ac1b662ee
Deathcaller120-0 <49953301+Deathcaller120-0@users.noreply.github.com>
2021-05-20 10:12:05 -0700

    Update detection.js

--- detection.js
+++ detection.js
@@ -1,3 +1,5 @@
+var debug = false;
+
 //window.alert("loading detection");
 
 function rectRect(x1, y1, w1, h1, x2, y2, w2, h2){
@@ -58,7 +60,7 @@ function pointPoint(x1, y1, x2, y2) {
 }
 //window.alert("pointPoint loaded");
 
-function lineLine(x1, y1, x2, y2, x3, y3, x4, y4) {
+function lineLine(x1, y1, x2, y2, x3, y3, x4, y4, inter = false) {
 
   // calculate the distance to intersection point
   let uA = ((x4-x3)*(y1-y3) - (y4-y3)*(x1-x3)) / ((y4-y3)*(x2-x1) - (x4-x3)*(y2-y1));
@@ -68,15 +70,19 @@ function lineLine(x1, y1, x2, y2, x3, y3, x4, y4) {
   if (uA >= 0 && uA <= 1 && uB >= 0 && uB <= 1) {
 
     // optionally, draw a circle where the lines meet
-    if(debug){
-      let intersectionX = x1 + (uA * (x2-x1));
-      let intersectionY = y1 + (uA * (y2-y1));
+    let intersectionX = x1 + (uA * (x2-x1));
+    let intersectionY = y1 + (uA * (y2-y1));
+    if (debug){  
       fill(255,0,0);
       noStroke();
       ellipse(intersectionX,intersectionY, 20,20);
     }
-
-    return true;
+      
+    if (inter){
+      return [intersectionX, intersectionY];
+    } else {
+      return true;
+    }
   }
   return false;
 }
@@ -99,3 +105,109 @@ function lineRect(x1, y1, x2, y2, rx, ry, rw, rh) {
   return false;
 }
 //window.alert("lineRect loaded");
+
+/*
+//========================\\
+||will need to change this||
+\\========================//
+*/
+function pointsToScreenCoords(points) {
+  let screenPoints = [];   // create output array
+  for (let i = 0; i < points.length; i++) {                  // go through all the points
+    let x = points[i].x;         // get the screen x coordinate
+    let y = points[i].y;
+    screenPoints[i] = createVector(x, y);
+  }
+  return screenPoints;
+}
+
+
+// POLYGON/POLYGON
+function polyPoly(p1, p2) {
+  
+  // go through each of the vertices, plus the next vertex in the list
+  let next = 0;
+  for (let current=0; current<p1.length; current++) {
+    
+    // get next vertex in list
+    // if we’ve hit the end, wrap around to 0
+    next = current+1;
+    if (next == p1.length) next = 0;
+    
+    // get the PVectors at our current position
+    // this makes our if statement a little cleaner
+    let vc = p1[current];    // c for “current”
+    let vn = p1[next];       // n for “next”
+    
+    // now we can use these two points (a line) to compare to the
+    // other polygon’s vertices using polyLine()
+    let collision = polyLine(p2, vc.x,vc.y,vn.x,vn.y);
+    if (collision) return true;
+    
+    // optional: check if the 2nd polygon is INSIDE the first
+    collision = polyPoint(p1, p2[0].x, p2[0].y);
+    if (collision) return true;
+  }
+  
+  return false;
+}
+
+
+// POLYGON/LINE
+function polyLine(vertices, x1, y1, x2, y2) {
+
+  // go through each of the vertices, plus the next vertex in the list
+  let next = 0;
+  for (let current=0; current<vertices.length; current++) {
+
+    // get next vertex in list
+    // if we’ve hit the end, wrap around to 0
+    next = current+1;
+    if (next == vertices.length) next = 0;
+
+    // get the PVectors at our current position
+    // extract X/Y coordinates from each
+    let x3 = vertices[current].x;
+    let y3 = vertices[current].y;
+    let x4 = vertices[next].x;
+    let y4 = vertices[next].y;
+
+    // do a Line/Line comparison
+    // if true, return ‘true’ immediately and stop testing (faster)
+    let hit = lineLine(x1, y1, x2, y2, x3, y3, x4, y4);
+    if (hit) {
+      return true;
+    }
+  }
+
+  // never got a hit
+  return false;
+}
+
+// POLYGON/POINT
+// used only to check if the second polygon is INSIDE the first
+function polyPoint(vertices, px, py) {
+  let collision = false;
+  
+  // go through each of the vertices, plus the next vertex in the list
+  let next = 0;
+  for (let current=0; current<vertices.length; current++) {
+    
+    // get next vertex in list
+    // if we’ve hit the end, wrap around to 0
+    next = current+1;
+    if (next == vertices.length) next = 0;
+    
+    // get the PVectors at our current position
+    // this makes our if statement a little cleaner
+    let vc = vertices[current];    // c for “current”
+    let vn = vertices[next];       // n for “next”
+    
+    // compare position, flip ‘collision’ variable back and forth
+    if ( ((vc.y > py && vn.y < py) || (vc.y < py && vn.y > py)) &&
+         (px < (vn.x-vc.x) * (py-vc.y) / (vn.y-vc.y) + vc.x) ) {
+            collision = !collision;
+    }
+  }
+  return collision;  
+}
