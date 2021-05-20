934e3d9451f2d3141d3c58504e798f73d243a812
Deathcaller120-0 <49953301+Deathcaller120-0@users.noreply.github.com>
2021-05-20 10:24:02 -0700

    Update shapes.js

--- shapes.js
+++ shapes.js
@@ -17,6 +17,8 @@ class phys_Shape {
     this.type = shapeType;
     
     this.index = 0;
+      
+    this.invertedVectors = [0,0];
     
     } catch (err){
       window.alert("Shape Setup | " + err.name + ":" + err.message)
@@ -85,39 +87,45 @@ class phys_Shape {
   }
   
   collsionUpdate(){
-    let type0 = this.getShapeType();
-    let type1 = other.getShapeType();
+    for (let i = 0; i < physObj.length; i++){
+      if (this.getIndex() == physObj[i].getIndex()) {continue};
+      
+      let other = physObj[i];
+      let type0 = this.getShapeType();
+      let type1 = other.getShapeType();
     
-    //window.alert("checking larger collsion");
-    if (rectRect(other.pos.x - other.size.x, other.pos.y - other.size.y, other.size.x * 2, other.size.y * 2, this.pos.x - this.size.x/2, this.pos.y - this.size.y, this.size.x * 2, this.size.y * 2)){
-      if (type0 == "circle" || type1 == "circle"){
-        if (type0 == type1) {
-          if (polyPoly(this.hitPoints, other.hitPoints)){
-            let hits = [];
-            for (let i = 0; i < this.hitPoints.length; i++){
-              if(!polyPoint(other.hitPoints, this.hitPoints[i].x, this.hitPoints[i].y)) { continue; }
-              hits.push(this.hitPoints[].label);
-            }
-            
-            let invY = 0;
-            let invX = 0;
-            for (let i = 0; i < hits.length; i++){
-              if (hits[i] == "top-left" || hits[i] == "top-mid" || hits[i] == "top-right" || hits[i] == "bottom-left" || hits[i] == "bottom-mid" || hits[i] == "bottom-right" && invY == 0){
-                invY = 1;
+      //window.alert("checking larger collsion");
+      if (rectRect(other.pos.x - other.size.x, other.pos.y - other.size.y, other.size.x * 2, other.size.y * 2, this.pos.x - this.size.x/2, this.pos.y - this.size.y, this.size.x * 2, this.size.y * 2)){
+        if (type0 == "circle" || type1 == "circle"){
+          if (type0 == type1) {
+            if (polyPoly(this.hitPoints, other.hitPoints)){
+              let hits = [];
+              for (let i = 0; i < this.hitPoints.length; i++){
+                if(polyPoint(other.hitPoints, this.hitPoints[i].x, this.hitPoints[i].y)) { 
+                  hits.push(this.hitPoints[].label);
+                }
               }
-              if (hits[i] == "top-left" || hits[i] == "mid-left" || hits[i] == "top-right" || hits[i] == "bottom-left" || hits[i] == "bottom-right" || hits[i] == "mid-right" && invX == 0){
-                invX = 1;
-              }
-            }
             
-            if (invX){
-              this.invertVel(1);
-            }
-            if (invY){
-              this.invertVel(0);
-            }
-            if (invX || invY){
-              this.pos.add(this.vel);
+              let invY = 0;
+              let invX = 0;
+              for (let i = 0; i < hits.length; i++){
+                if ((hits[i] == "top-left" || hits[i] == "top-mid" || hits[i] == "top-right" || hits[i] == "bottom-left" || hits[i] == "bottom-mid" || hits[i] == "bottom-right") && invY == 0){
+                  invY = 1;
+                }
+                if ((hits[i] == "top-left" || hits[i] == "mid-left" || hits[i] == "top-right" || hits[i] == "bottom-left" || hits[i] == "bottom-right" || hits[i] == "mid-right") && invX == 0){
+                  invX = 1;
+                }
+              }
+            
+              if (invX){
+                this.invertVel(1);
+              }
+              if (invY){
+                this.invertVel(0);
+              }
+              if (invX || invY){
+                this.pos.add(this.vel);
+              }
             }
           }
         }
@@ -130,14 +138,18 @@ class phys_Shape {
   }
   
   invertVel(isX){
+    
     //window.alert("inverted vel for: " + (isX ? "x" : "y"));
     try {
-    if (isX){
-      this.vel.x += (-this.vel.x) * this.bounce;
-      this.vel.x *= -1;
-    } else {
-      this.vel.y += (-this.vel.y) * this.bounce;
-      this.vel.y *= -1;
+    if (this.invertedVectors[isX] !== 1){
+      this.invertedVectors[isX] = 1;
+      if (isX){
+        this.vel.x += (-this.vel.x) * this.bounce;
+        this.vel.x *= -1;
+      } else {
+        this.vel.y += (-this.vel.y) * this.bounce;
+        this.vel.y *= -1;
+      }
     }
       
     } catch (err){
@@ -166,15 +178,16 @@ class phys_Circle extends phys_Shape {
     let top = -w/2;
     let right = w/2;
     let bottom = w/2;
+    let mid = 0
     
     this.hitPoints.push({x:left, y:top, label:"top-left", hit:0});
-    this.hitPoints.push({x:left, y:0, label:"mid-left", hit:0});
+    this.hitPoints.push({x:left, y:mid, label:"mid-left", hit:0});
     this.hitPoints.push({x:left, y:bottom, label:"bottom-left", hit:0});
-    this.hitPoints.push({x:0, y:bottom, label:"bottom-mid", hit:0});
+    this.hitPoints.push({x:mid, y:bottom, label:"bottom-mid", hit:0});
     this.hitPoints.push({x:right, y:bottom, label:"bottom-right", hit:0});
-    this.hitPoints.push({x:right, y:0, label:"mid-right", hit:0});
+    this.hitPoints.push({x:right, y:mid, label:"mid-right", hit:0});
     this.hitPoints.push({x:right, y:top, label:"top-right", hit:0});
-    this.hitPoints.push({x:0, y:top, label:"top-mid", hit:0});
+    this.hitPoints.push({x:mid, y:top, label:"top-mid", hit:0});
     
     } catch (err){
       window.alert("Circle Setup | " + err.name + ": " + err.message)
@@ -209,11 +222,13 @@ class phys_Circle extends phys_Shape {
     line(0,0,this.vel.x,this.vel.y);
       
     noStroke();
-    colorMode(HSB, this.hitPoints.length, 1, 1, 1);
+    colorMode('hsb', this.hitPoints.length, 1, 1, 1);
     for (let i = 0; i < this.hitPoints.length; i++){
-      fill(i, 1, 1, 1);
-      circle(this.hitPoints[i].x, this.hitPoints[i].y, 3);
-      text(this.hitPoints[i].label, -20, -20+(i*22));
+      if (this.hitPoints[i].hit == 1){
+        fill(i, 1, 1, 1);
+        circle(this.hitPoints[i].x, this.hitPoints[i].y, 3);
+        text(this.hitPoints[i].label, -20, -20 + (i * 22));
+      }
     }
     pop();
       
