57849c5f5b1a85faede36c6a1a76822f94182924
Deathcaller120-0 <49953301+Deathcaller120-0@users.noreply.github.com>
2021-05-28 10:29:16 -0700

    gravity i++

--- main.js
+++ main.js
@@ -73,7 +73,7 @@ function draw(){
   // if making a gravity well then render circles in size of well
   if (mouseIsPressed){
     fill(255, 10);
-    for (let i = 0; i < phys.gravityWellSize + 1; i += 5){
+    for (let i = 0; i < phys.gravityWellSize + 1; i++){
       circle(mouseX, mouseY, i * 10);
     }
   }
