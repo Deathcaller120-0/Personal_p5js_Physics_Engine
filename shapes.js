6af6cc5109e69f314795c123df6b422d8f4333df
Deathcaller120-0 <49953301+Deathcaller120-0@users.noreply.github.com>
2021-06-01 13:59:12 -0700

    reverted back to flooring

--- shapes.js
+++ shapes.js
@@ -220,7 +220,6 @@ class phys_Shape {
     for (let i = 0; i < this.hitPoints.length; i++){
       fill(i, 1, 1, 1);
       circle(this.hitPoints[i].x, this.hitPoints[i].y, 3);
-      //text(this.hitPoints[i].x.toPrecision(4) + ", " + this.hitPoints[i].y.toPrecision(4), (this.pos.x > width / 2 ? -this.size.x * 2 : this.size.x), -(this.hitPoints.length * 12) + (i * 13));
       text(this.hitPoints[i].x + ", " + this.hitPoints[i].y, (this.pos.x > width / 2 ? -this.size.x * 2 : this.size.x), -(this.hitPoints.length * 12) + (i * 13));
     }
     
@@ -257,8 +256,8 @@ class phys_Circle extends phys_Shape {
     }
     
     for (let i = 0; i < angles.length; i++){
-      angles[i].x = angles[i].x.toPrecision(4);
-      angles[i].y = angles[i].y.toPrecision(4);
+      angles[i].x = Math.floor(angles[i].x);
+      angles[i].y = Math.floor(angles[i].y);
     }
     
     for (let i = 0; i < angles.length; i++){
